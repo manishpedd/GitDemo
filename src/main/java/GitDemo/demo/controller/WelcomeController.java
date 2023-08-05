418e8b3583e9132281c954b50443ce49b6054b5a
manish <manish.peddurwar@gmail.com>
2023-08-05 18:19:52 +0530

    added updation in controller

--- src/main/java/GitDemo/demo/controller/WelcomeController.java
+++ src/main/java/GitDemo/demo/controller/WelcomeController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+   public static void main(String[] args) {
 
-   System.out.println("Hello");
+
+      System.out.println("Hello");
+
+      System.out.println("Hello");
+
+   }
 
 
 
